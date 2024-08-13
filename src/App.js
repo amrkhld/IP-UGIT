6dabf82461a93ead9b92e4baedfb320e1c0f715e
amr khalid <amrkhld.wrk@gmail.com>
2024-08-13 10:01:54 +0300

    Whole

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import NavC from "./components/layout/NavC";
+import Home from "./pages/home";
+import About from "./pages/about";
+import NotFound from "./pages/notFound";
+import FootC from "./components/layout/FootC";
+import User from "./pages/User";
+import { GithubProvider } from "./context/githubContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubProvider>
+      <div className="bgCont">
+        <div className="screen-box">
+          <Router>
+            <NavC />
+            <div className="content">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/404" element={<NotFound />} />
+                <Route path="/*" element={<Navigate to="/404" />} />
+              </Routes>
+            </div>
+            <FootC />
+          </Router>
+        </div>
+      </div>
+    </GithubProvider>
   );
 }
 
